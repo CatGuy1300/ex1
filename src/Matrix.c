beb5c9f41fe09f565d6cfbf68ade07de117cce13
Guy Vitrak <guyv22@gmail.com>
2020-08-11 19:13:15 +0300

    added notes and fixed bug in matrix creation

--- src/Matrix.c
+++ src/Matrix.c
@@ -12,22 +12,26 @@ typedef struct Matrix {
 const int MIN_INDEX_VAL = 0;
 
 ErrorCode matrix_create(PMatrix* matrix, uint32_t height, uint32_t width) {
-    if (height < MIN_INDEX_VAL || width < MIN_INDEX_VAL) {
+    //dims must be positive
+    if (height < MIN_INDEX_VAL + 1 || width < MIN_INDEX_VAL + 1) {
         return ERROR_NEG_INDEX;
     }
 
+    //better safe than sorry
     *matrix = NULL;
+
+    //allocating memory for struct
     *matrix = (PMatrix) malloc(sizeof(Matrix));
     (*matrix)->height = height;
     (*matrix)->width = width;
-    ((*matrix)->array) = (double**) malloc(height * sizeof(double*));
 
-    //creating the array repersenig the matrix
+     //creating the array repersenig the matrix
+    ((*matrix)->array) = (double**) malloc(height * sizeof(double*));
     if (((*matrix)->array) == NULL) {
         return ERROR_MEMORY;
     }
 
-    //
+    //creating the arrays inside the array (2d array)
     for (int i = 0; i < height; ++i) {
         ((*matrix)->array)[i] = (double*) calloc(width, sizeof(double));
         if (((*matrix)->array)[i] == NULL) {
@@ -39,113 +43,153 @@ ErrorCode matrix_create(PMatrix* matrix, uint32_t height, uint32_t width) {
 }
 
 ErrorCode matrix_copy(PMatrix* result, CPMatrix source) {
+    //checking for null
     if (source == NULL) {
         return ERROR_NULL;
     }
+
     //creating new matrix with same dimenstions
     ErrorCode error = matrix_create(result, source->height, source->width);
     if (!error_isSuccess(error)) {
         return error; 
     }
-    //copies data of source to result using
+
+    //copies data of source to result
     for (int i = 0; i < source->height; ++i) {
         for (int j = 0; j < source->width; ++j) {
             ((*result)->array)[i][j] = (source->array)[i][j];
         }
     }
+
     return ERROR_SUCCESS;
 }
 
 void matrix_destroy(PMatrix matrix) {
     if (matrix != NULL) {
+        //freeing 2d array
     for (int i = 0; i < matrix->height; ++i) {
             free((matrix->array)[i]);
          }
         free(matrix->array);
+
+        //freeing sturct
         free(matrix);
+
+        //better safe than sorry
         matrix = NULL;
     }
 }
 
 ErrorCode matrix_getHeight(CPMatrix matrix, uint32_t* result) {
+    //checking for null
     if (matrix == NULL) {
         return ERROR_NULL;
     }
+
+    //setting result to be height
     *result = matrix->height;
+
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_getWidth(CPMatrix matrix, uint32_t* result) {
+    //checking for null
     if (matrix == NULL) {
         return ERROR_NULL;
     }
+
+    //setting result to be height
     *result = matrix->width;
+
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_setValue(PMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
                           double value) {
+    //checking for null
     if (matrix == NULL) {
         return ERROR_NULL;
     }
+
+    //checking for invalid input
     if (rowIndex < MIN_INDEX_VAL || colIndex < MIN_INDEX_VAL) {
         return ERROR_NEG_INDEX;
     }
     if (rowIndex >= matrix->height || colIndex >= matrix->width) {
         return ERROR_OUT_OF_MATRIX_BOUNDS;
     }
+
+    //setting the value
     (matrix->array)[rowIndex][colIndex] = value;
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_getValue(CPMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
                           double* value) {
+    //checking for null
     if (matrix == NULL) {
         return ERROR_NULL;
     }
+
+    //checking for invalid input
     if (rowIndex < MIN_INDEX_VAL || colIndex < MIN_INDEX_VAL) {
         return ERROR_NEG_INDEX;
     }
     if (rowIndex >= matrix->height || colIndex >= matrix->width) {
         return ERROR_OUT_OF_MATRIX_BOUNDS;
     }
+
+    //setting value to be the value
     *value = (matrix->array)[rowIndex][colIndex];
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_add(PMatrix* result, CPMatrix lhs, CPMatrix rhs) {
+    //checking for null
     if (lhs == NULL || rhs == NULL) {
         return ERROR_NULL;
     }
+
+    //cheking if addition can be done (by definition)
     if (lhs->height != rhs->height ||
         lhs->width != rhs->width) {
             return ERROR_ADDITION_IS_NOT_DEFINED;
     }
+
+    //creating the result matrix
     ErrorCode error = matrix_create(result, lhs->height, lhs->width);
     if (!error_isSuccess(error)) {
         return error;
     }
     
+    //calculating the addition matrix by definition
     for (int i = 0; i < lhs->height; ++i) {
         for (int j = 0; j < lhs->width; ++j) {
             ((*result)->array)[i][j] = (lhs->array)[i][j] + (rhs->array)[i][j];
         }
     }
+
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_multiplyMatrices(PMatrix* result, CPMatrix lhs, CPMatrix rhs) {
+    //checking for null
     if (lhs == NULL || rhs == NULL) {
         return ERROR_NULL;
     }
+
+    //cheking if multiplication can be done (by definition)
     if (lhs->width != rhs->height) {
             return ERROR_MULTIPLICATION_IS_NOT_DEFINED;
         }
+
+    //creating the result matrix
     ErrorCode error = matrix_create(result, lhs->height, rhs->width);
     if (!error_isSuccess(error)) {
         return error;
     }
 
+    //calculating the multiplication matrix by definition
     for (int i = 0; i < lhs->height; ++i) {
         for (int j = 0; j < rhs->width; ++j) {
             double product = 0;
@@ -155,17 +199,22 @@ ErrorCode matrix_multiplyMatrices(PMatrix* result, CPMatrix lhs, CPMatrix rhs) {
             ((*result)->array)[i][j] = product;
         }
     }
+
     return ERROR_SUCCESS;
 }
 
 ErrorCode matrix_multiplyWithScalar(PMatrix matrix, double scalar) {
+    //checking for null
     if (matrix == NULL) {
         return ERROR_NULL;
     }
+
+    //calculating the multiplication matrix by definition
     for (int i = 0; i < matrix->height; ++i) {
         for (int j = 0; j < matrix->width; ++j) {
             (matrix->array)[i][j] *= scalar;
         }
     }
+
     return ERROR_SUCCESS;
 }
