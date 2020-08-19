9ea378438b1a5ecd291d8c44ab7ff6a73ebb2d92
Guy Vitrak <guyv22@gmail.com>
2020-08-19 12:29:55 +0300

    fixed C9, C11 in ErrorCode.c

--- src/ErrorCode.c
+++ src/ErrorCode.c
@@ -5,34 +5,31 @@ bool error_isSuccess(ErrorCode code) {
 }
 
 const char* error_getErrorMessage(ErrorCode code) {
-    const char* str;
     switch (code)
     {
     case ERROR_SUCCESS:
-        str = "There is no error";
+        return "There is no error";
         break;
     case ERROR_NULL:
-        str = "Input is null";
+        return "Input is null";
         break;
     case ERROR_SIZE_CANT_BE_ZERO:
-        str = "Matrix height and width mustn't be zero";
+        return "Matrix height and width mustn't be zero";
         break;
     case ERROR_OUT_OF_MATRIX_BOUNDS:
-        str = "The index you wanted to reach is out of matrix's bounds";
+        return "The index you wanted to reach is out of matrix's bounds";
         break;
     case ERROR_ADDITION_IS_NOT_DEFINED:
-        str = "Addition is not defined - the height and width of matrixes should be equal";
+        return "Addition is not defined - the height and width of matrixes should be equal";
         break;   
     case ERROR_MULTIPLICATION_IS_NOT_DEFINED:
-        str = "Multiplication is not defined - the width of first matrix must be equals to height of second matrix";
+        return "Multiplication is not defined - the width of first matrix must be equals to height of second matrix";
         break;  
     case ERROR_MEMORY:
-        str = "There is not enough memory";
+        return "There is not enough memory";
         break;  
     default:
-        str = "Unknown code";
+        return "Unknown code";
         break;
     }
-
-    return str;
 }
